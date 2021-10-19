cc6023c5f1327898830aede91d822c31b94134a0
TS93 <tschram93@yahoo.com>
2021-10-19 18:04:52 -0400

    saved keycode comments of values

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('keydown', function (e) {
+    console.log(e);
+});
+
+// A = keycode(65)
+// S = keycode(83)
+// D = keycode(68)
+// F = keycode(70)
+// G = keycode(71)
+// H = keycode(72)
+// J = keycode(74)
+// K = keycode(75)
+// L = keycode(76)
